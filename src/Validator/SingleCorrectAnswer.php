99af653f5c1d3441f5cf3e5dcc2a41a1f75ca569
Oumeyma <oumeymaradhouani1@gmail.com>
2026-02-07 21:10:35 +0100

    Quiz Module : added validator + verified all crud ops

---
+++ src/Validator/SingleCorrectAnswer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+final class SingleCorrectAnswer extends Constraint
+{
+    public $messageNoCorrect = 'A question must have at least one correct answer.';
+    public $messageTooManyCorrect = 'You can only select ONE correct answer.';
+    
+    // You can use #[HasNamedArguments] to make some constraint options required.
+    // All configurable options must be passed to the constructor.
+    public function __construct(
+        public string $mode = 'strict',
+        ?array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct([], $groups, $payload);
+    }
+}
