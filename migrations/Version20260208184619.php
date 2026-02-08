05d9a60305ff6e1e937f6fe068032a65ea693821
Nouha11 <nouha.nhme@gmail.com>
2026-02-08 21:37:08 +0100

    fixing templates

--- migrations/Version20260208184619.php
+++ migrations/Version20260208184619.php
@@ -10,11 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<<< HEAD:migrations/Version20260208182006.php
-final class Version20260208182006 extends AbstractMigration
-========
-final class Version20260208170854 extends AbstractMigration
->>>>>>>> 0c68414e8e11865d517f7be01f44385948957730:migrations/Version20260208170854.php
+final class Version20260208184619 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,7 +20,10 @@ final class Version20260208170854 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE books CHANGE price price NUMERIC(10, 2) DEFAULT NULL, CHANGE cover_image cover_image VARCHAR(255) DEFAULT NULL, CHANGE author author VARCHAR(255) DEFAULT NULL, CHANGE isbn isbn VARCHAR(20) DEFAULT NULL, CHANGE published_at published_at DATETIME DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+        $this->addSql('CREATE TABLE book_library (book_id INT NOT NULL, library_id INT NOT NULL, INDEX IDX_32A0B02A16A2B381 (book_id), INDEX IDX_32A0B02AFE2541D7 (library_id), PRIMARY KEY (book_id, library_id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE book_library ADD CONSTRAINT FK_32A0B02A16A2B381 FOREIGN KEY (book_id) REFERENCES books (id)');
+        $this->addSql('ALTER TABLE book_library ADD CONSTRAINT FK_32A0B02AFE2541D7 FOREIGN KEY (library_id) REFERENCES libraries (id)');
+        $this->addSql('ALTER TABLE books ADD price NUMERIC(10, 2) DEFAULT NULL, ADD cover_image VARCHAR(255) DEFAULT NULL, ADD author VARCHAR(255) DEFAULT NULL, ADD isbn VARCHAR(20) DEFAULT NULL, ADD published_at DATETIME DEFAULT NULL, ADD uploader_id INT DEFAULT NULL, ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE course CHANGE description description VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE libraries CHANGE address address VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE loans CHANGE end_at end_at DATETIME DEFAULT NULL');
@@ -39,7 +38,10 @@ final class Version20260208170854 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE books CHANGE price price NUMERIC(10, 2) DEFAULT \'NULL\', CHANGE cover_image cover_image VARCHAR(255) DEFAULT \'NULL\', CHANGE author author VARCHAR(255) DEFAULT \'NULL\', CHANGE isbn isbn VARCHAR(20) DEFAULT \'NULL\', CHANGE published_at published_at DATETIME DEFAULT \'NULL\', CHANGE updated_at updated_at DATETIME DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE book_library DROP FOREIGN KEY FK_32A0B02A16A2B381');
+        $this->addSql('ALTER TABLE book_library DROP FOREIGN KEY FK_32A0B02AFE2541D7');
+        $this->addSql('DROP TABLE book_library');
+        $this->addSql('ALTER TABLE books DROP price, DROP cover_image, DROP author, DROP isbn, DROP published_at, DROP uploader_id, DROP created_at, DROP updated_at');
         $this->addSql('ALTER TABLE course CHANGE description description VARCHAR(255) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE libraries CHANGE address address VARCHAR(255) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE loans CHANGE end_at end_at DATETIME DEFAULT \'NULL\'');
