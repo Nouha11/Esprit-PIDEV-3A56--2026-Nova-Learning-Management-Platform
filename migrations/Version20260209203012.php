18f87bda7cb0c8d56c56f37d1326303c0cb8fd5d
obenzinouba <obenzinouba@gmail.com>
2026-02-09 21:31:14 +0100

    added onetomany relation between user and courses

--- migrations/Version20260209203012.php
+++ migrations/Version20260209203012.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260209192057 extends AbstractMigration
+final class Version20260209203012 extends AbstractMigration
 {
     public function getDescription(): string
     {
