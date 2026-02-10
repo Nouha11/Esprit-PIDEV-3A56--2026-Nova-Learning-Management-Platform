196314af29e719c1f9a1a0d33c9d824f8c6192ce
Oumeyma <oumeymaradhouani1@gmail.com>
2026-02-10 13:40:17 +0100

    checking

--- migrations/Version20260210083507.php
+++ migrations/Version20260210083507.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260210050152 extends AbstractMigration
+final class Version20260210083507 extends AbstractMigration
 {
     public function getDescription(): string
     {
