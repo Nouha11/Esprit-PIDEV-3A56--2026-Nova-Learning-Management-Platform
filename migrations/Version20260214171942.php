8be5a3de1684d3146cd43fe6e0236cd2efb80069
Nouha11 <nouha.nhme@gmail.com>
2026-02-15 12:58:37 +0100

    removing required for all

--- migrations/Version20260214171942.php
+++ migrations/Version20260214171942.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260213134452 extends AbstractMigration
+final class Version20260214171942 extends AbstractMigration
 {
     public function getDescription(): string
     {
