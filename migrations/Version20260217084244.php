0c3ac80e3f5001b94dabebea4be3eec55b5492fa
aciljouini <aciljouini@gmail.com>
2026-02-17 09:49:09 +0100

    StudySession Branch

--- migrations/Version20260217084244.php
+++ migrations/Version20260217084244.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260216161552 extends AbstractMigration
+final class Version20260217084244 extends AbstractMigration
 {
     public function getDescription(): string
     {
